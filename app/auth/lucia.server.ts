b4de4eea2de6a0df41a23586d6608f33e064ae2a
Semere Tereffe <se.semere.tereffe@gmail.com>
2024-04-11 00:35:15 +0300

    Hot Fix: Add log

--- app/auth/lucia.server.ts
+++ app/auth/lucia.server.ts
@@ -9,6 +9,7 @@ const adapter = new DrizzlePostgreSQLAdapter(db, TB_sessions, TB_users);
 
 
 export const google = new Google(process.env.GOOGLE_CLIENT_ID!, process.env.GOOGLE_CLIENT_SECRET!, `${getBaseUrl()}/auth/signed-in-with-google`);
+console.log(`THIS IS THE REDIRECT URI   ${getBaseUrl()}/auth/signed-in-with-google`);
 
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {
