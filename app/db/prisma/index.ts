ffc9e2ab91f7d8e0c7540b2d9a0348e1fd999d57
Semere Tereffe <se.semere.tereffe@gmail.com>
2024-06-03 16:22:43 +0300

    Use turso adapter for sqlite

--- app/db/prisma/index.ts
+++ app/db/prisma/index.ts
@@ -1,4 +1,16 @@
-import {PrismaClient} from "@prisma/client";
+// import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+import { env } from "~/api";
 
-export const prisma = new PrismaClient();
+const libsql = createClient({
+  url: env.SQLITE_URL,
+  authToken: env.SQLITE_AUTHKEY ?? undefined,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+export const prisma = new PrismaClient({ adapter });
+// export const prisma = new PrismaClient();
+
 
