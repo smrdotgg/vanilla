08d9fa71b223893c79f05048175403ce47f70075
Semere Tereffe <se.semere.tereffe@gmail.com>
2024-06-18 03:45:41 +0300

    Simplify Domain Purchase User Info Form

--- app/server/api/routers/domains.ts
+++ app/server/api/routers/domains.ts
@@ -14,62 +14,28 @@ export const domainsRouter = createTRPCRouter({
   setDomainUserInfo: protectedProcedure
     .input(domainUserInfoZodSchema)
     .mutation(async ({ ctx, input }) => {
-      const data = {
-        userId: ctx.session.uid,
-        ...input,
-      };
       const user = await prisma.user.findFirst({
         where: { firebase_id: ctx.session.uid },
       });
-      const newData = {
-        user_id: user!.id,
-        tech_city: data.techCity,
-        admin_city: data.adminCity,
-        billing_city: data.billingCity,
-        tech_country: data.techCountry,
-        admin_country: data.adminCountry,
-        tech_address_1: data.techAddress1,
-        tech_last_name: data.techLastName,
-        admin_address_1: data.adminAddress1,
-        admin_last_name: data.adminLastName,
-        tech_first_name: data.techFirstName,
-        admin_first_name: data.adminFirstName,
-        billing_country: data.billingCountry,
-        registrant_city: data.registrantCity,
-        tech_postal_code: data.techPostalCode,
-        admin_postal_code: data.adminPostalCode,
-        billing_address_1: data.billingAddress1,
-        billing_last_name: data.billingLastName,
-        tech_phone_number: data.techPhoneNumber,
-        admin_phone_number: data.adminPhoneNumber,
-        billing_first_name: data.billingFirstName,
-        tech_email_address: data.techEmailAddress,
-        admin_email_address: data.adminEmailAddress,
-        billing_postal_code: data.billingPostalCode,
-        registrant_country: data.registrantCountry,
-        tech_state_province: data.techStateProvince,
-        admin_state_province: data.adminStateProvince,
-        billing_phone_number: data.billingPhoneNumber,
-        registrant_address_1: data.registrantAddress1,
-        registrant_last_name: data.registrantLastName,
-        registrant_first_name: data.registrantFirstName,
-        billing_email_address: data.billingEmailAddress,
-        billing_state_province: data.billingStateProvince,
-        registrant_postal_code: data.registrantPostalCode,
-        tech_phone_country_code: data.techPhoneCountryCode,
-        admin_phone_country_code: data.adminPhoneCountryCode,
-        registrant_phone_number: data.registrantPhoneNumber,
-        registrant_email_address: data.registrantEmailAddress,
-        billing_phone_country_code: data.billingPhoneCountryCode,
-        registrant_state_province: data.registrantStateProvince,
-        registrant_phone_country_code: data.registrantPhoneCountryCode,
-      };
+      const data = {
+          user_id: user!.id,
+          city: input.registrantCity,
+          address: input.registrantAddress1,
+          country: input.registrantCountry,
+          last_name: input.registrantLastName,
+          first_name: input.registrantFirstName,
+          postal_code: input.registrantPostalCode,
+          phone_number: input.registrantPhoneNumber,
+          email_address: input.registrantEmailAddress,
+          phone_code: input.registrantPhoneCountryCode,
+          state_province: input.registrantStateProvince,
+        };
       await prisma.domain_purchase_form_info.upsert({
         where: {
           user_id: user!.id,
         },
-        update: newData,
-        create: newData,
+        update: data,
+        create: data,
       });
     }),
   purchaseDomain: protectedProcedure
@@ -94,23 +60,23 @@ export const domainsRouter = createTRPCRouter({
     .query(async ({ input, ctx }) => {
       const batchSize = 25;
 
-      const dbCall = await ctx.db.select({ count: count() }).from(TB_contacts);
-      const ids = (await ctx.db.select().from(TB_contacts)).map((c) => c.id);
+      // const dbCall = await ctx.db.select({ count: count() }).from(TB_contacts);
+      // const ids = (await ctx.db.select().from(TB_contacts)).map((c) => c.id);
 
-      const contactCount = dbCall[0].count;
-      const pageCount = Math.ceil(contactCount / batchSize);
+      // const contactCount = dbCall[0].count;
+      // const pageCount = Math.ceil(contactCount / batchSize);
 
       return {
-        data: await ctx.db
-          .select()
-          .from(TB_contacts)
-          .limit(batchSize)
-          .offset((input.cursor - 1) * batchSize),
+        // data: await ctx.db
+        //   .select()
+        //   .from(TB_contacts)
+        //   .limit(batchSize)
+        //   .offset((input.cursor - 1) * batchSize),
         cursor: input.cursor,
         batchSize,
-        pageCount,
-        contactCount,
-        ids,
+        // pageCount,
+        // contactCount,
+        // ids,
       };
     }),
   delete: publicProcedure
@@ -123,17 +89,17 @@ export const domainsRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       if (input.mode === "all") {
         if (input.exceptions.length > 0) {
-          await ctx.db
-            .delete(TB_contacts)
-            .where(notInArray(TB_contacts.id, input.exceptions));
+          // await ctx.db
+          //   .delete(TB_contacts)
+          //   .where(notInArray(TB_contacts.id, input.exceptions));
         } else {
-          await ctx.db.delete(TB_contacts);
+          // await ctx.db.delete(TB_contacts);
         }
       } else if (input.mode === "none") {
         if (input.exceptions.length > 0) {
-          await ctx.db
-            .delete(TB_contacts)
-            .where(inArray(TB_contacts.id, input.exceptions));
+          // await ctx.db
+          //   .delete(TB_contacts)
+          //   .where(inArray(TB_contacts.id, input.exceptions));
         } else {
           // nothing selected
         }
--- app/services/domain.server/NameCheapDomainService.ts
+++ app/services/domain.server/NameCheapDomainService.ts
@@ -24,51 +24,51 @@ export const NameCheapDomainService: DomainServiceInterface = {
     const apiData: { [key: string]: string | number | null } = {
       DomainName: domainName,
       Years: 1,
-      RegistrantFirstName: detail.registrant_first_name,
-      RegistrantLastName: detail.registrant_last_name,
-      RegistrantAddress1: detail.registrant_address_1,
-      RegistrantCity: detail.registrant_city,
-      RegistrantStateProvince: detail.registrant_state_province,
-      RegistrantPostalCode: detail.registrant_postal_code,
-      RegistrantCountry: detail.registrant_country,
+      RegistrantFirstName: detail.first_name,
+      RegistrantLastName: detail.last_name,
+      RegistrantAddress1: detail.address,
+      RegistrantCity: detail.city,
+      RegistrantStateProvince: detail.state_province,
+      RegistrantPostalCode: detail.postal_code,
+      RegistrantCountry: detail.country,
       RegistrantPhone:
-        detail.registrant_phone_country_code +
+        detail.phone_code +
         "." +
-        detail.registrant_phone_number,
-      RegistrantEmailAddress: detail.registrant_email_address,
+        detail.phone_number,
+      RegistrantEmailAddress: detail.email_address,
 
-      TechFirstName: detail.tech_first_name,
-      TechLastName: detail.tech_last_name,
-      TechAddress1: detail.tech_address_1,
-      TechCity: detail.tech_city,
-      TechStateProvince: detail.tech_state_province,
-      TechPostalCode: detail.tech_postal_code,
-      TechCountry: detail.tech_country,
+      TechFirstName: detail.first_name,
+      TechLastName: detail.last_name,
+      TechAddress1: detail.address,
+      TechCity: detail.city,
+      TechStateProvince: detail.state_province,
+      TechPostalCode: detail.postal_code,
+      TechCountry: detail.country,
       TechPhone:
-        detail.tech_phone_country_code + "." + detail.tech_phone_number,
-      TechEmailAddress: detail.tech_email_address,
+        detail.phone_code + "." + detail.phone_number,
+      TechEmailAddress: detail.email_address,
 
-      AdminFirstName: detail.admin_first_name,
-      AdminLastName: detail.admin_last_name,
-      AdminAddress1: detail.admin_address_1,
-      AdminCity: detail.admin_city,
-      AdminStateProvince: detail.admin_state_province,
-      AdminPostalCode: detail.admin_postal_code,
-      AdminCountry: detail.admin_country,
+      AdminFirstName: detail.first_name,
+      AdminLastName: detail.last_name,
+      AdminAddress1: detail.address,
+      AdminCity: detail.city,
+      AdminStateProvince: detail.state_province,
+      AdminPostalCode: detail.postal_code,
+      AdminCountry: detail.country,
       AdminPhone:
-        detail.admin_phone_country_code + "." + detail.admin_phone_number,
-      AdminEmailAddress: detail.admin_email_address,
+        detail.phone_code + "." + detail.phone_number,
+      AdminEmailAddress: detail.email_address,
 
-      AuxBillingFirstName: detail.billing_first_name,
-      AuxBillingLastName: detail.billing_last_name,
-      AuxBillingAddress1: detail.billing_address_1,
-      AuxBillingCity: detail.billing_city,
-      AuxBillingStateProvince: detail.billing_state_province,
-      AuxBillingPostalCode: detail.billing_postal_code,
-      AuxBillingCountry: detail.billing_country,
+      AuxBillingFirstName: detail.first_name,
+      AuxBillingLastName: detail.last_name,
+      AuxBillingAddress1: detail.address,
+      AuxBillingCity: detail.city,
+      AuxBillingStateProvince: detail.state_province,
+      AuxBillingPostalCode: detail.postal_code,
+      AuxBillingCountry: detail.country,
       AuxBillingPhone:
-        detail.billing_phone_country_code + "." + detail.billing_phone_number,
-      AuxBillingEmailAddress: detail.billing_email_address,
+        detail.phone_code + "." + detail.phone_number,
+      AuxBillingEmailAddress: detail.email_address,
     };
 
     // literally just making TS happy
@@ -192,7 +192,7 @@ interface GetDomainDataApiResponse {
                   };
                 },
                 {
-                  LockDetails: {};
+                  LockDetails: object;
                 },
                 {
                   Whoisguard: {
